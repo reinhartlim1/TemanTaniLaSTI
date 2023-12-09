d24c7143d6a00ee582a96b6e8c8f7f0f22b5dd8a
reinhartlim <18221154@std.stei.itb.ac.id>
2023-12-09 10:00:43 +0700

    addmiddleware

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 from .routers import auth, user, orders, materials, payment
 from .database import engine
 from . import models
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(auth.router)
 app.include_router(user.router)
