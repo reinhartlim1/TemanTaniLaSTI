ca0538baa2677a3ed804d4c143d386bf1964454f
reinhartlim <18221154@std.stei.itb.ac.id>
2023-12-09 09:25:20 +0700

    change schemas

--- backend/schemas.py
+++ backend/schemas.py
@@ -57,11 +57,9 @@ class TokenData(BaseModel):
 
 class UserOut(BaseModel):
     id: int
-    username: str
     email: EmailStr
 
 class UserCreate(BaseModel):
-    username: str
     email: EmailStr
     password: str
 
