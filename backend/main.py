0467e8cfa53b463e12711ed0775fed43f60113e1
reinhartlim <18221154@std.stei.itb.ac.id>
2023-12-09 11:56:20 +0700

    add warehouse

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import auth, user, orders, materials, payment
+from .routers import auth, user, orders, materials, payment, warehouse
 from .database import engine
 from . import models
 from fastapi.middleware.cors import CORSMiddleware
@@ -21,6 +21,7 @@ app.include_router(user.router)
 app.include_router(orders.router)
 app.include_router(materials.router)
 app.include_router(payment.router)
+app.include_router(warehouse.router)
 
 @app.get("/")
 def root():
