de2304b53a08591622821062bc6925887b60f5e5
reinhartlim <18221154@std.stei.itb.ac.id>
2023-12-09 10:21:17 +0700

    login register

--- frontend/app/loginregister/page.tsx
+++ frontend/app/loginregister/page.tsx
@@ -1,26 +1,103 @@
+"use client";
 import Image from "next/image";
 
 import {
-    Stack,
-    Text,
-    Box,
-    Tabs,
-    Tab,
-    TabPanels,
-    TabList,
-    TabPanel,
-    Card,
-    Input,
-    Button,
-  } from '@chakra-ui/react'
-  
+  Stack,
+  Text,
+  Box,
+  Tabs,
+  Tab,
+  TabPanels,
+  TabList,
+  TabPanel,
+  Card,
+  Input,
+  Button,
+  useToast,
+} from "@chakra-ui/react";
+import axios from "../axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
+export default function Register() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const toast = useToast();
+  const handleSignUp = async () => {
+    try {
+      const response = await axios.post("/users/", {
+        email,
+        password,
+      });
+      toast({
+        title: "Sign Up success",
+        description: "Logging in...",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+        position: "top",
+      });
+      handleLogin();
+    } catch (error) {
+      toast({
+        title: "Sign Up failed",
+        description: "Email has been used",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+        position: "top",
+      });
+    }
+  };
 
-  
-  export default function Register(){
-    return(
-    <Stack align='center' width="100%" height="720px" maxWidth="100%" background="green.50">  
-      <Text style={{margin: '50px 0px' }}
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        "/login",
+        {
+          username: email,
+          password: password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      toast({
+        title: "Login success",
+        description: "Welcome back",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+        position: "top",
+      });
+      localStorage.setItem("token", response.data.access_token);
+      localStorage.setItem("token_type", response.data.token_type);
+      router.push("/material");
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Login failed",
+        description: "Email or password is wrong",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+        position: "top",
+      });
+    }
+  };
+  return (
+    <Stack
+      align="center"
+      width="100%"
+      height="720px"
+      maxWidth="100%"
+      background="green.50"
+    >
+      <Text
+        style={{ margin: "50px 0px" }}
         fontFamily="Inter"
         lineHeight="1.2"
         fontWeight="bold"
@@ -29,9 +106,9 @@ import {
       >
         Selamat Datang di Website Teman Tani!
       </Text>
-      
+
       <Stack
-        align='center'
+        align="center"
         borderRadius="24px"
         borderColor="gray.200"
         borderStartWidth="1px"
@@ -44,29 +121,26 @@ import {
         background="white"
         boxShadow="base"
       >
-        <Box pt='2'>
-        <Image style={{ textAlign: 'center', margin: '20px 0px' }}
+        <Box pt="2">
+          <Image
+            style={{ textAlign: "center", margin: "20px 0px" }}
             alt="Logo Teman Tani"
             src={"/teman-tani.png"}
             className="image"
             width={75}
             height={74}
             priority
-        />
+          />
         </Box>
-        <Tabs 
-        align="center"
-        mx={20}
-        >
+        <Tabs align="center" mx={20}>
           <TabList>
             <Tab>Daftar</Tab>
             <Tab>Masuk</Tab>
           </TabList>
 
-        <TabPanels>  
-          <TabPanel>
+          <TabPanels>
+            <TabPanel>
               <Card width="100%" height="330px" maxWidth="100%">
-
                 <Text
                   fontFamily="Inter"
                   lineHeight="1.5"
@@ -76,79 +150,93 @@ import {
                 >
                   Email
                 </Text>
-                <Input style={{ margin: '0px px 20px 0px'}} 
+                <Input
+                  style={{ margin: "0px px 20px 0px" }}
                   placeholder="Email"
                   size="lg"
                   width=""
                   height="48px"
                   maxWidth="100%"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
 
                 <Text
-                fontFamily="Inter"
-                lineHeight="1.5"
-                fontWeight="regular"
-                fontSize="14px"
-                color="black"
+                  fontFamily="Inter"
+                  lineHeight="1.5"
+                  fontWeight="regular"
+                  fontSize="14px"
+                  color="black"
                 >
                   Password
                 </Text>
-                <Input style={{ margin: '0px 0px 20px 0px'}} 
+                <Input
+                  style={{ margin: "0px 0px 20px 0px" }}
                   placeholder="Password"
                   size="lg"
                   width="371px"
                   height="48px"
                   maxWidth="100%"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
 
-                <Button colorScheme="orange">Register</Button>
-              </Card>  
-          </TabPanel>
+                <Button colorScheme="orange" onClick={handleSignUp}>
+                  Register
+                </Button>
+              </Card>
+            </TabPanel>
 
-          <TabPanel>
-          <Card width="371px" height="330px" maxWidth="100%">
-            <Text
-              fontFamily="Inter"
-              lineHeight="1.5"
-              fontWeight="regular"
-              fontSize="14px"
-              color="black"
-            >
-            Email
-            </Text>
-            <Input style={{ margin: '0px 0px 50px 0px'}}
-              placeholder="Username"
-              size="lg"
-              width="371px"
-              height="48px"
-              maxWidth="100%"
-            />
+            <TabPanel>
+              <Card width="371px" height="330px" maxWidth="100%">
+                <Text
+                  fontFamily="Inter"
+                  lineHeight="1.5"
+                  fontWeight="regular"
+                  fontSize="14px"
+                  color="black"
+                >
+                  Email
+                </Text>
+                <Input
+                  style={{ margin: "0px 0px 50px 0px" }}
+                  placeholder="Username"
+                  size="lg"
+                  width="371px"
+                  height="48px"
+                  maxWidth="100%"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
 
-            <Text
-              fontFamily="Inter"
-              lineHeight="1.5"
-              fontWeight="regular"
-              fontSize="14px"
-              color="black"
-            >
-              Password
-            </Text>
-            <Input style={{ margin: '0px 0px 50px 0px'}}
-              placeholder="Password"
-              size="lg"
-              width="371px"
-              height="48px"
-              maxWidth="100%"
-            />
+                <Text
+                  fontFamily="Inter"
+                  lineHeight="1.5"
+                  fontWeight="regular"
+                  fontSize="14px"
+                  color="black"
+                >
+                  Password
+                </Text>
+                <Input
+                  style={{ margin: "0px 0px 50px 0px" }}
+                  placeholder="Password"
+                  size="lg"
+                  width="371px"
+                  height="48px"
+                  maxWidth="100%"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
 
-            <Button colorScheme="orange">Login</Button>
-            </Card>  
-          </TabPanel>
-
-        </TabPanels>
+                <Button colorScheme="orange" onClick={handleLogin}>
+                  Login
+                </Button>
+              </Card>
+            </TabPanel>
+          </TabPanels>
         </Tabs>
-        
       </Stack>
     </Stack>
-  )
-}  
+  );
+}
---
+++ frontend/components/navbar.tsx
@@ -0,0 +1,46 @@
+'use client'
+import { usePathname } from "next/navigation";
+import NextLink from 'next/link'
+import { Link } from '@chakra-ui/react'
+import Image from "next/image";
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+
+export default function Navbar(){
+    return (
+        <div className="box">
+              <div className="navbar">
+                <Box
+                    w={"100%"}
+                    display={"flex"}
+                    alignItems={"center"}
+                    justifyContent={"space-between"}
+                  >
+                    <Text fontSize={"x-large"} fontWeight={"bold"}>
+                      Welcome, Tito Sutadi
+                    </Text>
+                    <Box position={"relative"}>
+                      <Image
+                        objectFit="cover"
+                        width={50}
+                        height={50}
+                        src="/avatar.png"
+                        alt="Caffe Latte"
+                      />
+                      <Box
+                        position={"absolute"}
+                        right={0}
+                        bottom={1}
+                        w={4}
+                        h={4}
+                        borderWidth={2}
+                        borderColor={"white"}
+                        bg={"green"}
+                        borderRadius={"50%"}
+                      ></Box>
+                    </Box>
+                  </Box>           
+              </div>          
+            </div>
+    )
+}
