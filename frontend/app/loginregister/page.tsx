0d89a4ef4d1cbdd36f18c209aa0bbc04f91e029d
reinhartlim <18221154@std.stei.itb.ac.id>
2023-12-09 09:46:30 +0700

    test home page

---
+++ frontend/app/loginregister/page.tsx
@@ -0,0 +1,154 @@
+import Image from "next/image";
+
+import {
+    Stack,
+    Text,
+    Box,
+    Tabs,
+    Tab,
+    TabPanels,
+    TabList,
+    TabPanel,
+    Card,
+    Input,
+    Button,
+  } from '@chakra-ui/react'
+  
+
+
+  
+  export default function Register(){
+    return(
+    <Stack align='center' width="100%" height="720px" maxWidth="100%" background="green.50">  
+      <Text style={{margin: '50px 0px' }}
+        fontFamily="Inter"
+        lineHeight="1.2"
+        fontWeight="bold"
+        fontSize="36px"
+        color="black"
+      >
+        Selamat Datang di Website Teman Tani!
+      </Text>
+      
+      <Stack
+        align='center'
+        borderRadius="24px"
+        borderColor="gray.200"
+        borderStartWidth="1px"
+        borderEndWidth="1px"
+        borderTopWidth="1px"
+        borderBottomWidth="1px"
+        width="768px"
+        height="600px"
+        maxWidth="100%"
+        background="white"
+        boxShadow="base"
+      >
+        <Box pt='2'>
+        <Image style={{ textAlign: 'center', margin: '20px 0px' }}
+            alt="Logo Teman Tani"
+            src={"/teman-tani.png"}
+            className="image"
+            width={75}
+            height={74}
+            priority
+        />
+        </Box>
+        <Tabs 
+        align="center"
+        mx={20}
+        >
+          <TabList>
+            <Tab>Daftar</Tab>
+            <Tab>Masuk</Tab>
+          </TabList>
+
+        <TabPanels>  
+          <TabPanel>
+              <Card width="100%" height="330px" maxWidth="100%">
+
+                <Text
+                  fontFamily="Inter"
+                  lineHeight="1.5"
+                  fontWeight="regular"
+                  fontSize="14px"
+                  color="black"
+                >
+                  Email
+                </Text>
+                <Input style={{ margin: '0px px 20px 0px'}} 
+                  placeholder="Email"
+                  size="lg"
+                  width=""
+                  height="48px"
+                  maxWidth="100%"
+                />
+
+                <Text
+                fontFamily="Inter"
+                lineHeight="1.5"
+                fontWeight="regular"
+                fontSize="14px"
+                color="black"
+                >
+                  Password
+                </Text>
+                <Input style={{ margin: '0px 0px 20px 0px'}} 
+                  placeholder="Password"
+                  size="lg"
+                  width="371px"
+                  height="48px"
+                  maxWidth="100%"
+                />
+
+                <Button colorScheme="orange">Register</Button>
+              </Card>  
+          </TabPanel>
+
+          <TabPanel>
+          <Card width="371px" height="330px" maxWidth="100%">
+            <Text
+              fontFamily="Inter"
+              lineHeight="1.5"
+              fontWeight="regular"
+              fontSize="14px"
+              color="black"
+            >
+            Email
+            </Text>
+            <Input style={{ margin: '0px 0px 50px 0px'}}
+              placeholder="Username"
+              size="lg"
+              width="371px"
+              height="48px"
+              maxWidth="100%"
+            />
+
+            <Text
+              fontFamily="Inter"
+              lineHeight="1.5"
+              fontWeight="regular"
+              fontSize="14px"
+              color="black"
+            >
+              Password
+            </Text>
+            <Input style={{ margin: '0px 0px 50px 0px'}}
+              placeholder="Password"
+              size="lg"
+              width="371px"
+              height="48px"
+              maxWidth="100%"
+            />
+
+            <Button colorScheme="orange">Login</Button>
+            </Card>  
+          </TabPanel>
+
+        </TabPanels>
+        </Tabs>
+        
+      </Stack>
+    </Stack>
+  )
+}  
---
+++ frontend/app/page.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6" style={{ backgroundImage: 'url(/bg2.png)', backgroundSize: 'cover' }}>
+      <div className="flex justify-center items-center h-40 p-4 rounded-20">
+      </div>
+      <div className="flex items-center bg-gray-400 p-10 m-1 mt-[-20px] flex-col bg-opacity-70 rounded-20">
+  
+  <div className=" p-8 rounded-lg bg-opacity-80 rounded-20">
+    <p className="text-center text-orange-800 text-5xl font-bold font-inter"style={{ fontFamily: 'cursive' }}>Welcome to FlowerPath</p>
+  </div>
+  <div className="flex items-center justify-center mt-4">
+        <Link
+          href="/loginregister"
+          className="mx-auto flex items-center justify-center gap-5 self-start rounded-lg bg-green-600 px-6 py-5 text-sm font-medium text-white transition-colors hover:bg-green-500 md:text-base"
+        >
+          <span>Menuju Halaman Login</span>
+        </Link>
+      </div>
+</div>
+
+      <div className="mt-5 flex flex-col gap-4 md:flex-row justify-center items-center p-5">
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* Konten untuk bagian ini */}
+        </div>
+      </div>
+    </main>
+  );
+}
